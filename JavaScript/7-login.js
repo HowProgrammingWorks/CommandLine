527edcf3846febba6e5c11684d64ec5b3f714225
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-25 18:59:42 +0300

    Add console example using streams

---
+++ JavaScript/7-login.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const write = s => process.stdout.write(s);
+
+process.stdin.on('data', chunk => {
+  write(`\nHello, ${chunk.toString().trim()}!`);
+});
+
+write('\x1Bc');
+write('\x1b[10;10H');
+
+setTimeout(() => process.exit(0), 10000);
+
+write(`
+                      ┌────────────────────────────┐
+                      │ Login:                     │
+                      └────────────────────────────┘
+`);
+
+write('\x1b[2A\x1b[31C');
