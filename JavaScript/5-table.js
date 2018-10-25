626a1d5791fa1cb1411962e6b5ea21d0e2a6d6dc
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-25 18:54:24 +0300

    Add more examples

---
+++ JavaScript/5-table.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const persons = [
+  { name: 'Marcus Aurelius', city: 'Rome', born: 121 },
+  { name: 'Victor Glushkov', city: 'Rostov on Don', born: 1923 },
+  { name: 'Ibn Arabi', city: 'Murcia', born: 1165 },
+  { name: 'Mao Zedong', city: 'Shaoshan', born: 1893 },
+  { name: 'Rene Descartes', city: 'La Haye en Touraine', born: 1596 },
+];
+
+console.table(persons);
---
+++ JavaScript/6-group.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('Level 1');
+console.group();
+console.log('Level 2');
+console.group();
+console.log('Level 3');
+console.groupEnd();
+console.log('Message 1');
+console.groupEnd();
+console.log('Message 2');
