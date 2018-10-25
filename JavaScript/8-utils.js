ccaffd431e28ab6150f3e6ab3b2c3fcccd9f9e2c
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-25 23:57:48 +0300

    Implement console utilities

---
+++ JavaScript/8-utils.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Utilities
+
+const write = s => process.stdout.write(s);
+const read = callback => process.stdin.on('data', chunk => {
+  callback(chunk.toString().trim());
+});
+const clear = () => write('\x1Bc');
+const pos = (row, col) => write(`\x1b[${row};${col}H`);
+const box = (row, col, height, width) => {
+  const h = height - 2;
+  const w = width - 2;
+  const border = '┌┐─│└┘'.split('');
+  pos(row, col);
+  write(border[0] + border[2].repeat(w) + border[1]);
+  pos(row + h, col);
+  for (let i = 1; i < h; i++) {
+    pos(row + i, col);
+    write(border[3] + ' '.repeat(w) + border[3]);
+  }
+  pos(row + h, col);
+  write(border[4] + border[2].repeat(w) + border[5]);
+};
+
+// Uasage
+
+read(s => {
+  write(`\nHello, ${s}!\n`);
+  process.exit(0);
+});
+clear();
+setTimeout(() => process.exit(0), 10000);
+box(10, 10, 4, 30);
+pos(11, 12);
+write('Login: ');
